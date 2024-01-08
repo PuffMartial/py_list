5d50a6422a462e0e4ed0fa9c359cc1f03d1703c4
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2024-01-08 09:16:19 +0200

    Add files via upload

---
+++ pactist.py
@@ -0,0 +1,10 @@
+# list
+fruits =["orange", "strawbarry", "pineappile", "peach"]
+vagetable = ["corn", "onion", "carrot"]
+# appending
+fruits.append("watermelon")
+# insert
+fruits.insert(2,"apple")
+# mergin two list
+fruits.extend(vagetable)
+print(fruits)
